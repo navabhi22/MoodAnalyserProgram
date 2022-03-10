09d2ef6d77b2b490f6720a12e8683e82efd31b15
navabhi22 <navabhi22@gmail.com>
2022-03-10 21:01:16 +0530

    [Abhinav] Added methhod to analyse mood from message

---
+++ MoodAnalyserProblem/MoodAnalyser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblem
+{
+    public class MoodAnalyser
+    {
+        /// <summary>
+        /// Analysing mood from message
+        /// </summary>
+        public string message;
+
+        //Constructor for initializing the message
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        //Method to analyse mood from a given message
+        public string AnalyseMood()
+        {
+            if (this.message.ToLower().Contains("sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
