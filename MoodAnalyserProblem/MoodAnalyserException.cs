6b8feff0ebfd9472a3d0112e907d8c40b4777a6c
navabhi22 <navabhi22@gmail.com>
2022-03-10 22:21:24 +0530

    [Abhinav] Added Custom Exception handling for null and empty input, UC-3, TC-1,2

---
+++ MoodAnalyserProblem/MoodAnalyserException.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace MoodAnalyserProblem
+{
+    public class MoodAnalyserException : Exception
+    {
+        public ExceptionTypes type;
+
+        //Enum to differentiate mood analysis errors
+        public enum ExceptionTypes
+        {
+            NULL_MOOD_EXCEPTION,
+            EMPTY_MOOD_EXCEPTION
+        }
+
+        //Constructor to initialize ExceptionTypes
+        public MoodAnalyserException(ExceptionTypes type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
