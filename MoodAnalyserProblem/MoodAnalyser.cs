25a89f1a02f1c98ecb1b2ea6d99af41d3d4daf6d
navabhi22 <navabhi22@gmail.com>
2022-03-10 21:24:09 +0530

    [Abhinav] Added Exception handling for null input

---
+++ MoodAnalyserProblem/MoodAnalyser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblem
+{
+    public class MoodAnalyser
+    {
+        public string message;
+
+        //Constructor for initializing the message
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        //Method to analyse mood form a given message
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (this.message.ToLower().Contains("sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch (NullReferenceException)
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
