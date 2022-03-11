98c0c3ad4b2452b623a4d07bc7d6c97de5dbabb3
navabhi22 <navabhi22@gmail.com>
2022-03-11 16:33:34 +0530

    [Abhinav] Added MoodAnalyseFactory.cs

---
+++ MoodAnalyserProblem/MoodAnalyseFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblem
+{
+    public class MoodAnalyserFactory
+    {
+        /// <summary>
+        /// default Constructor - Create MoodAnalyserFactory and specify static method to create MoodAnalyser Object
+        /// </summary>
+        /// <param name="className"></param>
+        /// <param name="constructorName"></param>
+        /// <returns></returns>
+        /// <exception cref="MoodAnalyserException"></exception>
+        public static object CreateMoodAnalyse(string className, string constructorName)
+        {
+            string pattern = @"." + constructorName + "$";
+            Match result = Regex.Match(className, pattern);
+            if (result.Success)
+            {
+                try
+                {
+                    Assembly executing = Assembly.GetExecutingAssembly();
+                    Type moodAnalyserType = executing.GetType(className);
+                    return Activator.CreateInstance(moodAnalyserType);
+                }
+                catch (ArgumentNullException)
+                {
+
+                    throw new MoodAnalyserException(MoodAnalyserException.ExceptionTypes.NO_SUCH_CLASS, "Class not found");
+                }
+            }
+            else
+            {
+                throw new MoodAnalyserException(MoodAnalyserException.ExceptionTypes.NO_SUCH_METHOD, "Constructor not found");
+            }
+        }
+    }
+}
