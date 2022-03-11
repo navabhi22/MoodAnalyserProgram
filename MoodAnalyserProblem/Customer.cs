99a972abb715d9be4e5f24ec8a60064f3c2d15b4
navabhi22 <navabhi22@gmail.com>
2022-03-11 16:32:51 +0530

    [Abhinav] Added Customer.cs

---
+++ MoodAnalyserProblem/Customer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblem
+{
+    public class Customer
+    {
+        
+        //Properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        
+
+        public Customer(int Id, string name)
+        {
+            this.Id = Id;
+            this.Name = name;
+        }
+
+        public Customer()
+        {
+            this.Id = -1;
+            this.Name = String.Empty;
+        }
+        //Method To Print Id
+        public void PrintId()
+        {
+            Console.WriteLine("Id is : {0}", this.Id);
+        }
+
+        //Method To Print Name
+        public void PrintName()
+        {
+            Console.WriteLine("Name is : {0}", this.Name);
+        }
+    }
+}
